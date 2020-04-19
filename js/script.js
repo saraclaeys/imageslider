7073ffcc74c23334d6d328422d773fc7c3cb0c34
saraclaeys <sara.claeys01@gmail.com>
2020-04-19 10:08:50 +0200

    variables js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+// Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
