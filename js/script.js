c4bd127a15e085e33f2b266140e1fbe59ed9a8a2
saraclaeys <sara.claeys01@gmail.com>
2020-04-19 10:45:13 +0200

    event listeners clones

--- js/script.js
+++ js/script.js
@@ -20,6 +20,19 @@ nextBtn.addEventListener('click', () => {
 
 prevBtn.addEventListener('click', () => {
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
+    counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitioned', () => {
+    if (carouselImages[counter].id === 'lastClone') {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (carouselImages[counter].id === 'firstClone') {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
 });
