7c733466ccbad625c80c546da60e3108413993dd
saraclaeys <sara.claeys01@gmail.com>
2020-04-19 10:18:16 +0200

    counter

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ const carouselImages = document.querySelectorAll('.carousel-slide img');
 // Buttons
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
+
+// Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
